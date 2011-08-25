65b44583f61857d7d8d45ae017e4683387861e5b
MonkeyTaolun <tchai@shutterstock.com>
2011-08-25 16:33:37 -0400

    test

--- cpp/md5/test.cpp
+++ cpp/md5/test.cpp
@@ -6,8 +6,9 @@
 
 int main(int argc, char** argv){
 
-	cv::Mat image = cv::imread("test_image.jpg");
-
+	// cv::Mat image = cv::imread("test_image.jpg");
+  cv::Mat image;
+  image.release();
 	//std::cout << "md5 of grape is : " << md5("grape") << std::endl;	
 
 	MD5 md;
@@ -25,15 +26,17 @@ int main(int argc, char** argv){
   //std::cout << "we got " << image.elemSize() * image.total() << std::endl; 
   // md.update(image.data, 1000);
 	md.update((char *)image.data, image.elemSize() * image.total());
-	md.finalize();
-  std::string ss((char *)image.data, 20);
-  ss += "SUFT";
-  md2.update(image.data, 20);
+	md2.update((char *)NULL,0);
+  md.finalize();
+  //std::string ss((char *)image.data, 20);
+  //ss += "SUFT";
+  //md2.update(image.data, 20);
   md2.finalize();
-  std::string hex_test = md.hexdigest() + md2.hexdigest();
-	std::cout << md5(ss) << std::endl;
-  //std::cout << hex_test << std::endl;
-  
+  std::string hex_test = md.hexdigest();
+
+	//std::cout << md5(ss) << std::endl;
+  std::cout << hex_test << std::endl;
+  std::cout << md2.hexdigest() << std::endl;
 	return 0;
 
 }
